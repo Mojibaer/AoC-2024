6e52a16ade0f7c247098f7d0879a6030dc3ba43f
Moji <hassan.erfani@tao.at>
2024-12-05 22:49:20 +0100

    Day-2: Part two - not working

--- day-2/main.go
+++ day-2/main.go
@@ -42,7 +42,8 @@ func main() {
 		log.Fatal("Scanner thrown an Error: ", err)
 	}
 
-	fmt.Println(partOne(lines))
+	//fmt.Println(partOne(lines))
+	fmt.Println("function two", partTwo(lines))
 }
 
 func partOne(lines [][]int) int {
@@ -71,4 +72,151 @@ func partOne(lines [][]int) int {
 	}
 
 	return safeCounter
+}
+
+func partTwo(lines [][]int) int {
+	safeCounter := 0
+
+	for _, line := range lines {
+		increase := false
+		decrease := false
+		tolerateCounter := 0
+
+		if increaseLine(line) || decreaseLine(line) {
+			safeCounter++
+			continue
+		}
+
+		for i := 0; i < len(line)-1; i++ {
+
+			x := line[i] - line[i+1]
+
+			// Case if x is out of range
+			if (x < -3) || (x > 3) {
+				tolerateCounter++
+
+				if tolerateCounter > 1 {
+					break
+				}
+
+				newLine := getNewLineWithoutCurrentIndex(line, i+1)
+
+				if tolerateCounter < 2 && (increaseLine(newLine) || decreaseLine(newLine)) {
+					safeCounter++
+				}
+			}
+
+			// Case if x is zero
+			if x == 0 {
+				tolerateCounter++
+
+				newLine := getNewLineWithoutCurrentIndex(line, i)
+
+				if tolerateCounter < 2 && (increaseLine(newLine) || decreaseLine(newLine)) {
+					safeCounter++
+				}
+
+				break
+			}
+
+			// Case if sorting switches from increase to decrease
+			if (x > 0) && ((x) < 4) && increase {
+				tolerateCounter++
+
+				newLine := getNewLineWithoutCurrentIndex(line, i)
+
+				if tolerateCounter < 2 && (increaseLine(newLine) || decreaseLine(newLine)) {
+					safeCounter++
+				}
+
+				break
+			}
+
+			// Case if sorting switches from decrease to increase
+			if (x < 0) && ((x) > -4) && decrease {
+				tolerateCounter++
+
+				newLine := getNewLineWithoutCurrentIndex(line, i)
+
+				if tolerateCounter < 2 && (increaseLine(newLine) || decreaseLine(newLine)) {
+					safeCounter++
+				}
+
+				break
+			}
+
+			// TolerateCounter check on increase
+			if (x < 0) && ((x) > -4) {
+				if decrease {
+					tolerateCounter++
+
+					if tolerateCounter > 1 {
+						break
+					}
+				}
+
+				increase = true
+				decrease = false
+			}
+
+			// TolerateCounter check on decrease
+			if (x > 0) && ((x) < 4) {
+				if increase {
+					tolerateCounter++
+
+					if tolerateCounter > 1 {
+						break
+					}
+				}
+
+				increase = false
+				decrease = true
+			}
+
+			if i == len(line)-1 && tolerateCounter < 2 {
+				safeCounter++
+			}
+		}
+	}
+
+	return safeCounter
+}
+
+func increaseLine(line []int) bool {
+
+	for i := 0; i < len(line)-1; i++ {
+		x := line[i+1] - line[i]
+
+		if !(x > 0 && x < 4) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func decreaseLine(line []int) bool {
+	for i := 0; i < len(line)-1; i++ {
+		x := line[i] - line[i+1]
+
+		if !(x > 0 && x < 4) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func getNewLineWithoutCurrentIndex(line []int, index int) []int {
+	newLine := make([]int, 0, len(line)-1)
+
+	for i, value := range line {
+		if i == index {
+			continue
+		}
+
+		newLine = append(newLine, value)
+	}
+
+	return newLine
 }
